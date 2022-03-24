1c71bc7ed7a23894df62352667e4677b2308b248
Firas Jelassi <49401280+Friscas@users.noreply.github.com>
2022-03-24 20:58:49 +0100

    added events

--- index.js
+++ index.js
@@ -6,15 +6,21 @@
  *
  */
 
+/* eslint-disable no-await-in-loop */
+
+/* eslint-disable node/prefer-promises/fs */
+
 const EventEmitter = require('events');
 const fs = require('fs');
 const dns = require('dns').promises;
 const axios = require('axios').default;
 const IPCIDR = require('ip-cidr');
 
+const event = new EventEmitter();
+
 class isCloudy {
-  constructor(host, update) {
-    this.ip = this._validateHost(host);
+  constructor(target, update) {
+    this.ip = this._validateHost(target);
     this.update = update || false;
     this.IPs = [];
   }
@@ -30,13 +36,29 @@ class isCloudy {
 
     if (this.IPs.length > 0) {
       // very bad coding ik, i will improve the code quality once i am done with the main tool im working on
-      return await this._isCloudy();
+      const results = await this._isCloudy();
+      if (results) event.emit('done', results);
+      return results;
     }
+  }
+
+  onError(method) {
+    event.addListener('error', method);
+  }
+
+  onCloudFlareFound(method) {
+    event.addListener('found', method);
+  }
+
+  onDone(method) {
+    event.addListener('done', method);
   }
 
   _validateHost(host) {
     if (typeof host !== 'string' && !Array.isArray(host)) {
-      throw new TypeError('Invalid type');
+      throw new TypeError(
+        'Invalid type for the target, it can only be a string or an array of strings'
+      );
     }
 
     return host;
@@ -51,9 +73,7 @@ class isCloudy {
         const ranges = data.data.split('\n');
         this._generateIPs(ranges);
       })
-      .catch((err) => {
-        debugger;
-      });
+      .catch((err) => event.emit('error', err));
   }
 
   _generateIPs(ranges) {
@@ -69,7 +89,7 @@ class isCloudy {
   _writeArrayToJsonFile(ips) {
     fs.writeFile('ips.json', JSON.stringify(ips), 'utf8', (err) => {
       if (err) {
-        // call error event
+        event.emit('error', err);
       } else {
         this.update = false;
         this.check(); // ik know, i know its very bad, i will handle every async as it should be correctly handled once i start the rework
@@ -81,27 +101,47 @@ class isCloudy {
     return !fs.existsSync('ips.json');
   }
 
+  _isURL(url) {
+    try {
+      url = new URL(url);
+    } catch (error) {
+      return false;
+    }
+    return url.host;
+  }
+
   async _ValidateIp(ip) {
-    ip = await dns.lookup(ip);
-    return ip.address;
+    try {
+      if (this._isURL(ip)) {
+        ip = this._isURL(ip);
+      }
+      ip = await dns.lookup(ip);
+      return ip.address;
+    } catch (error) {
+      event.emit('error', error);
+    }
   }
 
   async _isCloudy() {
     let results = [];
     let bFound;
+    let result;
     if (typeof this.ip === 'string') {
       bFound = this.IPs.includes(await this._ValidateIp(this.ip));
       results = {
         target: this.ip,
         CloudFlare: bFound
       };
+      if (bFound) event.emit('found', results);
     } else {
       for (const ip of this.ip) {
         bFound = this.IPs.includes(await this._ValidateIp(ip));
-        results.push({
+        result = {
           target: ip,
           CloudFlare: bFound
-        });
+        };
+        results.push(result);
+        if (bFound) event.emit('found', result);
       }
     }
 
--- test/test.js
+++ test/test.js
@@ -1,9 +1,26 @@
 // const test = require('ava');
 const Cloudy = require('../index');
 
-const cloudflare = new Cloudy('104.21.68.253', false);
+const cloudflare = new Cloudy(
+  [
+    '188.114.96.7',
+    'api.linkino.me',
+    'https://admin.linkino.me/',
+    'https://linkino.me/'
+  ],
+  false
+);
 
 (async function () {
+  cloudflare.onError((err) => {
+    debugger;
+  });
+  cloudflare.onCloudFlareFound((data) => {
+    debugger;
+  });
+  cloudflare.onDone((data) => {
+    debugger;
+  });
   const result = await cloudflare.check();
   console.log(result);
 })();
