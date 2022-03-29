d8423ac24cb11aa1ec17ee99514aa52ddc7abc48
Firas Jelassi <49401280+Friscas@users.noreply.github.com>
2022-03-29 20:50:02 +0200

    removed .history dir

--- .history/test/test_20220323223511.js
+++
@@ -1,5 +0,0 @@
-const test = require('ava');
-
-const Script = require('..');
-pt._name, 'script');
-});
--- .history/test/test_20220323223524.js
+++
@@ -1,6 +0,0 @@
-//const test = require('ava');
-
-const Cloudy = require('../index');
-const cloudflare = new Cloudy('1.1.1.1', true);
-
-cloudflare.check();
